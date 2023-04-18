f7bb3ca75f5a6d7e30842cb9e9121bef5fd8b23d
NilKad <kadulin@gmail.com>
2023-04-19 02:02:36 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+// import './index.css';
 import App from 'components/App';
 import { GlobalStyle } from 'components/BaseStyles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/practice-tweet-card">
+    <BrowserRouter basename="/practice_tweet-card">
       <GlobalStyle />
       <App />
     </BrowserRouter>
