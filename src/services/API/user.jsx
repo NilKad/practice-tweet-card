bff376e6de24a4c0c561ef80f862f82f5fde13ef
NilKad <kadulin@gmail.com>
2023-04-19 14:37:20 +0300

    fix/git-pages

--- src/services/API/user.jsx
+++ src/services/API/user.jsx
@@ -4,11 +4,9 @@ const getFollowers = () => {
   try {
     if (localStorage.key('tweetUser')) {
       const data = JSON.parse(localStorage.getItem('tweetUser'))
-      if (!data) {
-        localStorage.setItem('tweetUser', JSON.stringify([]))
-        return []
+      if (data) {
+        return data;
       }
-      return data;
     }
     localStorage.setItem('tweetUser', JSON.stringify([]))
     return []
