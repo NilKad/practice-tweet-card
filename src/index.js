45dae4c6845f03f26b620330e353d9f3a0af331a
NilKad <kadulin@gmail.com>
2023-04-20 17:46:26 +0300

    fix404

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { GlobalStyle } from 'components/BaseStyles/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/practice_tweet-card">
       <GlobalStyle />
       <App />
     </BrowserRouter>
