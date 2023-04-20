bdafbf35bc74215dc74a6d734cf23b68d00dd212
NilKad <kadulin@gmail.com>
2023-04-20 18:17:41 +0300

    fix404

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, Router } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 import TweetsPage from 'pages/TweetPage';
 import TweetsLayout from 'layouts/TweetsLayout';
@@ -6,14 +6,16 @@ import TweetsLayout from 'layouts/TweetsLayout';
 function App() {
   // const toHome = () => {};
   return (
-    <Routes>
-      <Route path="/" element={<TweetsLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="tweets" element={<TweetsPage />} />
-        {/* <Route path="*" element={<Navigate to="/" />} /> */}
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace={true} />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TweetsLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="tweets" element={<TweetsPage />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+    </Router>
   );
 }
 
